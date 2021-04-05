62e973ddbaf394757d8bd91fd98ba80a4781ffa0
sagajayakumar <jayakumar1994@gmail.com>
2021-04-05 00:04:01 -0500

    mongoose connection

--- models/car.js
+++ models/car.js
@@ -5,3 +5,42 @@ const carSchema = mongoose.Schema({
     Price: String
 })
 module.exports = mongoose.model("Car", carSchema)
+
+
+// We can seed the collection if needed on server start
+async function recreateDB() {
+    // Delete everything
+    await car.deleteMany();
+    let instance1 = new car({
+        name: "Rouge",
+        Company: "Nissan",
+        Price: "$24000"
+    });
+    let instance2 = new car({
+        name: "Tahoe",
+        Company: "Chevrolet",
+        Price: "$49000"
+    });
+    let instance3 = new car({
+        name: "Santa Fe",
+        Company: "Hyundai",
+        Price: "$28000"
+    });
+
+    instance1.save(function(err, doc) {
+        if (err) return console.error(err);
+        console.log("First object saved")
+    });
+
+    instance2.save(function(err, doc) {
+        if (err) return console.error(err);
+        console.log("Second object saved")
+    });
+
+    instance3.save(function(err, doc) {
+        if (err) return console.error(err);
+        console.log("Third object saved")
+    });
+}
+let reseed = true;
+if (reseed) { recreateDB(); }
