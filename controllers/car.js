fb97f77ea415154af309a405df481c69cfc17960
sagajayakumar <jayakumar1994@gmail.com>
2021-04-09 13:28:06 -0500

    update api

--- controllers/car.js
+++ controllers/car.js
@@ -62,10 +62,11 @@ exports.car_detail = async function(req, res) {
 
 // Handle car update form on PUT.
 exports.car_update_put = async function(req, res) {
-    console.log(`update on id ${req.params.Name} with body ${JSON.stringify(req.body)}`)
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
     try {
-        let toUpdate = await car.findById(req.params.Name)
+        let toUpdate = await car.findById(req.params.id)
             // Do updates of properties
+        if (req.body.Name) toUpdate.Name = req.body.Name;
         if (req.body.Company) toUpdate.Company = req.body.Company;
         if (req.body.Price) toUpdate.Price = req.body.Price;
         let result = await toUpdate.save();
